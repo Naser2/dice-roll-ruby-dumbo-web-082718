9b023767431e2e9ceeb496cfc84c94bfbde44cdb
Nasser Sanou <nassersanou23@gmail.com>
2018-08-19 03:47:05 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,19 +3,22 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
-#this works with ranges
-# def roll(min, max)
-#   return rand(min..max)
+#With argument and Range
+def roll(min, max)
+  return rand(min..max)
   
-# end 
+end 
   
-# puts roll(1,6)
+puts roll(1,6)
 
-# array = [1, 2, 3, 4, 5, 6]
-# def roll(array)
-#   return rand(array.length).to_i
-# end 
-# roll(array)
+#With Arrays
+array = [1, 2, 3, 4, 5, 6]
+def roll(array)
+  return rand(array.length).to_i
+end 
+roll(array)
+
+
 def roll
  rand(1..6)
 end
